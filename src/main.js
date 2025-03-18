7150158393f67476b09d2c33107072470e75a966
Luscky724scorpion <songhokhan@gmail.com>
2025-03-18 00:24:31 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+let pokemon=[
+  "fire","water","grass"
+
+  
+
+]
+
+let fire=[
+   {gen1:"Charmander",
+    gen3:"Torchick",
+    gen2:"Cyndaquil"
+      
+
+   },
+ 
+
+]
+let water=[
+   {  
+    gen1:"Squirtle",
+       gen2:"Totodile",
+       gen3:"Mudkip",
+      
+
+   
+  }
+]
+let grass=[
+   {  
+      
+        gen1:"Bulbasaur",
+        gen2:"Chikorita",
+       gen3:"Treeko"
+
+   
+  }
+]
+
+let gens=
+pokemon.filter((pokemon) => pokemon.fire==="gen1"
+);
+console.log("gens")
